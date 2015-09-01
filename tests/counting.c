e47b464f61dcf8d58da511ec62535596dbee1987
Abhishek Kulkarni <adkulkar@indiana.edu>
2015-09-01 18:42:09 -0400

    Add a pointer to the counting example

--- tests/counting.c
+++ tests/counting.c
@@ -7,6 +7,7 @@ typedef struct counter_t counter_t;
 struct counter_t {
     unsigned int count;
     size_t limit;
+    int *pcount;
 };
 
 static counter_t ctr;
@@ -14,16 +15,20 @@ static counter_t ctr;
 int main(int argc, char* argv[])
 {
     ctr.limit = 120;
+    ctr.pcount = malloc(sizeof(int));
+    *ctr.pcount = 0;
     if (argc > 1)
         ctr.limit = atoi(argv[1]);
-  
+
     printf("Counting demo starting with pid %d\n", (int)getpid());
 
     int i;
     for (i = 0; i < ctr.limit; ++i) {
-        printf("Count = %d\n", ctr.count);
+        printf("Count = %d Pcount = %d\n", ctr.count, *ctr.pcount);
         sleep(2);
         ++ctr.count;
+        *ctr.pcount = 2*(ctr.count);
     }
+    free(ctr.pcount);
     return 0;
 }
